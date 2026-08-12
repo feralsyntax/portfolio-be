10d6e5ce43f48f34ed47bc5b28ce10da0fb1a57f
feralsyntax <fullstack.benie@gmail.com>
2026-08-12 21:13:32 +0300

    add (admin): customize the project model for the admin interface

--- portfolio_app/admin.py
+++ portfolio_app/admin.py
@@ -11,7 +11,6 @@ from portfolio_app.models import (
     Technology,
 )
 
-admin.site.register(Project)
 admin.site.register(Technology)
 admin.site.register(Feature)
 admin.site.register(Industry)
@@ -19,3 +18,31 @@ admin.site.register(Detail)
 admin.site.register(KeyFeature)
 admin.site.register(Challenge)
 admin.site.register(Impact)
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "industry",
+        "is_featured",
+        "date_added",
+    )
+
+    list_filter = (
+        "is_featured",
+        "industry",
+    )
+
+    search_fields = (
+        "name",
+        "short_description",
+        "long_description",
+    )
+
+    filter_horizontal = (
+        "technologies",
+        "features",
+    )
+
+    ordering = ("name",)
