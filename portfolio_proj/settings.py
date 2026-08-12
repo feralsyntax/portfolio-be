d04b27699e02bca4c412d6446099d82ef92f02ad
feralsyntax <fullstack.benie@gmail.com>
2026-08-12 18:27:55 +0300

    add (settings): add pk config

--- portfolio_proj/settings.py
+++ portfolio_proj/settings.py
@@ -144,3 +144,8 @@ MAILERS = {
         "BACKEND": "django.core.mail.backends.console.EmailBackend",
     },
 }
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
