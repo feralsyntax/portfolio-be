ce37540898eedabb4dcc5cfebccd535e96959dc5
feralsyntax <fullstack.benie@gmail.com>
2026-08-12 21:09:26 +0300

    add (admin): register models

--- portfolio_app/admin.py
+++ portfolio_app/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from portfolio_app.models import (
+    Challenge,
+    Detail,
+    Feature,
+    Impact,
+    Industry,
+    KeyFeature,
+    Project,
+    Technology,
+)
+
+admin.site.register(Project)
+admin.site.register(Technology)
+admin.site.register(Feature)
+admin.site.register(Industry)
+admin.site.register(Detail)
+admin.site.register(KeyFeature)
+admin.site.register(Challenge)
+admin.site.register(Impact)
