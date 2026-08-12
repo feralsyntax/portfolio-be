6ee1fc357e5cda28951387fd3afec81d970d5913
feralsyntax <fullstack.benie@gmail.com>
2026-08-12 18:21:52 +0300

    change (settings): remove default db configs

--- portfolio_proj/settings.py
+++ portfolio_proj/settings.py
@@ -96,17 +96,6 @@ TEMPLATES = [
 WSGI_APPLICATION = "portfolio_proj.wsgi.application"
 
 
-# Database
-# https://docs.djangoproject.com/en/6.1/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/6.1/ref/settings/#auth-password-validators
 
