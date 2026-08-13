f376ef562843aea2be1e54ecdcde63e34cfbc4dd
feralsyntax <fullstack.benie@gmail.com>
2026-08-13 09:54:47 +0300

    add (views): create a project view set to handle querying projects

--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from portfolio_app.models import Project
+from portfolio_app.serializers import ProjectSerializer
+
+
+class ProjectViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = ProjectSerializer
+
+    def get_queryset(self):
+        return Project.objects.select_related(
+            "industry",
+            "details",
+        ).prefetch_related(
+            "technologies",
+            "features",
+            "details__key_features",
+            "details__challenges",
+            "details__impacts",
+        )
