5425c1e1daeacbbee333033cf008b5136cc098bd
feralsyntax <fullstack.benie@gmail.com>
2026-08-13 11:42:48 +0300

    update (models): add a constraint for the is_featured field

--- portfolio_app/models.py
+++ portfolio_app/models.py
@@ -191,5 +191,13 @@ class Project(models.Model):
     class Meta:
         ordering: typing.ClassVar = ["name"]
 
+        constraints = [  # noqa: RUF012
+            models.UniqueConstraint(
+                fields=["is_featured"],
+                condition=models.Q(is_featured=True),
+                name="only_one_featured_project",
+            ),
+        ]
+
     def __str__(self):
         return self.name
