a2fe1d5865348c7309c9539579aaf72c496a15db
feralsyntax <fullstack.benie@gmail.com>
2026-08-22 18:18:36 +0300

    update (settings): add whitenoise middleware

--- portfolio_proj/settings.py
+++ portfolio_proj/settings.py
@@ -77,6 +77,7 @@ AUTH_USER_MODEL = "portfolio_app.CustomUser"
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "corsheaders.middleware.CorsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
