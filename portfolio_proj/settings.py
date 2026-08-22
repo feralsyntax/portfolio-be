5a5bfc8d8f58544851d8e1403d22e815b22af1e6
feralsyntax <fullstack.benie@gmail.com>
2026-08-22 17:50:20 +0300

    change (settings): update static files configs

--- portfolio_proj/settings.py
+++ portfolio_proj/settings.py
@@ -194,7 +194,6 @@ cloudinary.config(
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
